ab7a12ba575d42e12c1b409fd26ebce37236e75b
EC2 Default User <ec2-user@ip-172-31-37-15.us-east-2.compute.internal>
2020-09-19 05:34:02 +0000

    part 2 complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,13 +4,11 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
-Then /(.*) seed movies should exist/ do | n_seeds |
-  Movie.count.should be n_seeds.to_i
-end
+
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
@@ -25,14 +23,21 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
+
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if uncheck == "un"							#Condition for unchecked movies
+    rating_list.split(', ').each {|x| step %{I uncheck "ratings_#{x}"}} #splitting the ratings on the basis of comma
+  else									#Conition for checked movies
+    rating_list.split(', ').each {|x| step %{I check "ratings_#{x}"}}
+  end
 end
+
+
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  expect(page).to have_css("table#movies tbody tr", count: Movie.count)
 end
